6a47cfc70363120fe2a47508e6462792c14ee86a
hhlitval <litvinhh@gmail.com>
2023-07-15 18:46:31 +0200

    Fix GetDirectoryFilesViewModel

--- ViewModels/GetDirectoryFilesViewModel.cs
+++ ViewModels/GetDirectoryFilesViewModel.cs
@@ -10,19 +10,26 @@ using File_Analyzer.Models;
 namespace File_Analyzer.ViewModels
 {
     public class GetDirectoryFilesViewModel : BaseViewModel
-    {        
+    {
+        private const string LogFilePath = "log.txt";
         public IEnumerable<FileItem>? FileItems { get; set; }
 
         public GetDirectoryFilesViewModel(string folderPath)
         {
             DirectoryInfo dir = new DirectoryInfo(folderPath);
-            IEnumerable<FileInfo> fileList = dir.GetFiles("*.*", SearchOption.AllDirectories);
+            IEnumerable<FileInfo> fileList = dir.EnumerateFiles("*.*", new EnumerationOptions { IgnoreInaccessible = true, RecurseSubdirectories = true});
             FileItems = fileList.Select(fileInfo => new FileItem
             {
                 FileName = fileInfo.Name,
                 FilePath = fileInfo.DirectoryName,
                 FileSize = fileInfo.Length
             }).OrderByDescending(x => x.FileSize);        
-        }        
+        }
+
+        private void LogExceptionToFile(Exception ex, string filePath = "")
+        {
+            string logMessage = $"[{DateTime.Now}] Exception: {ex.Message}\nFilePath: {filePath}\nStackTrace: {ex.StackTrace}\n";
+            File.AppendAllText(LogFilePath, logMessage);
+        }
     }            
 }
